0f28ca215e6d2ddcd74b7b838fc03695d7ff2fcf
Dima <tzd0409@gmail.com>
2016-06-26 22:59:50 +0000

    Styling show.ejs

--- YelpCamp/seeds.js
+++ YelpCamp/seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Голубые озера",
         image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQmRGOYh6RqTja75yY2D5EMYxrvNRTq4aYHLTOkYg1KuS_443Z4",
-        description: "Хороший отдых на озерах. Чистый лес и прозрачная вода."
+        description: "Lorem ipsum dolor sit amet, pri an tale convenire, te mel debet tollit expetenda, vim eu phaedrum scribentur appellantur. Mei at denique accommodare vituperatoribus. Mel eu facer menandri, id phaedrum pericula partiendo vel. Voluptatum vituperatoribus vis id, ius ne eirmod propriae constituto. Ne nobis fabellas vis. Zril labores erroribus nam ea.Munere viderer suavitate no eam, facete fabellas dignissim ut eos, an mel dicta semper inimicus. Est facer audiam ut, at vis quod impedit dissentiunt. No putant moderatius suscipiantur usu. Nonumes nusquam mediocrem qui te. Has et facilisis periculis. Eu cum erant placerat dissentias, ea vis prompta comprehensam."
     },
     {
         name: "Оскол",
         image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRRv9OsEwQSFMq9SFTjLbz_Y_s55rav_BJbI4h_pTWqzKTkVOs0WQ",
-        description: "Здесь можно словит большую рыбу."
+        description: "Lorem ipsum dolor sit amet, pri an tale convenire, te mel debet tollit expetenda, vim eu phaedrum scribentur appellantur. Mei at denique accommodare vituperatoribus. Mel eu facer menandri, id phaedrum pericula partiendo vel. Voluptatum vituperatoribus vis id, ius ne eirmod propriae constituto. Ne nobis fabellas vis. Zril labores erroribus nam ea.Munere viderer suavitate no eam, facete fabellas dignissim ut eos, an mel dicta semper inimicus. Est facer audiam ut, at vis quod impedit dissentiunt. No putant moderatius suscipiantur usu. Nonumes nusquam mediocrem qui te. Has et facilisis periculis. Eu cum erant placerat dissentias, ea vis prompta comprehensam.ую рыбу."
     },
     {
         name: "Морской отдых",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYmfYo5mzcixMZPBj4Egv5unLb4gF50Md-eWwj3UQSUfz-71Ky",
-        description: "Море, пляж"
+        description: "Lorem ipsum dolor sit amet, pri an tale convenire, te mel debet tollit expetenda, vim eu phaedrum scribentur appellantur. Mei at denique accommodare vituperatoribus. Mel eu facer menandri, id phaedrum pericula partiendo vel. Voluptatum vituperatoribus vis id, ius ne eirmod propriae constituto. Ne nobis fabellas vis. Zril labores erroribus nam ea.Munere viderer suavitate no eam, facete fabellas dignissim ut eos, an mel dicta semper inimicus. Est facer audiam ut, at vis quod impedit dissentiunt. No putant moderatius suscipiantur usu. Nonumes nusquam mediocrem qui te. Has et facilisis periculis. Eu cum erant placerat dissentias, ea vis prompta comprehensam."
     }
 ];
 
